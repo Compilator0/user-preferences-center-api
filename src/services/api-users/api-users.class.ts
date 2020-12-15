f45b5e2c1f919a7aa7c51ec5132ed0cab3e81866
Compilator <junidryves@yahoo.fr>
2020-12-15 09:04:07 +0100

    testing

--- src/services/api-users/api-users.class.ts
+++ src/services/api-users/api-users.class.ts
@@ -1,6 +1,8 @@
 import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
 import { Application } from '../../declarations';
 
+
+// This class if for authentication service on top of the API via JWT Tokens 
 export class ApiUsers extends Service {
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
